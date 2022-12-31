196c95e66832d0f5407f179d3205c946921f518d
pidanmeng <1092333914@qq.com>
2022-12-31 10:04:44 +0800

    feat: 🎸 init

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Versions from './components/Versions.vue'
+</script>
+
+<template>
+  <Versions></Versions>
+</template>
+
+<style lang="less">
+@import './assets/css/styles.scss';
+</style>
